cdfe628cc3b035b74e2890e8a29507ac40ccef2e
tmrna <r.deans@outlook.com>
2023-06-09 11:41:36 -0400

    added layout for main

---
+++ word_counter/src/main.rs
@@ -0,0 +1,14 @@
+use std::{env, fs::read_to_string, process::exit};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("No file was provided");
+        exit(1);
+    }
+    let file_path = &args[1];
+    let contents = read_to_string(file_path).expect("Could not open file");
+    //let mut counter = WordCounter::default();
+    //counter.insert(&contents);
+    //counter.print();
+}
